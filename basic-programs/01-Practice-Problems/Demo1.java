ae74ec7d948a0417e114b6c8d0487dd2941a211e
shrsrasthikumari-creator <shrsrasthikumari@gmail.com>
2026-01-21 12:43:04 +0530

    added new branch

---
+++ basic-programs/01-Practice-Problems/Demo1.java
@@ -0,0 +1,7 @@
+publi class Demo1{
+public static void at(); 
+
+public static void main(String[] args) {
+System.out.printIn("HelloWorld");
+}
+} 
---
+++ basic-programs/02-Java-Primitive-Data/Demo2.java
@@ -0,0 +1,7 @@
+publi class Demo2{
+public static void at()
+
+public static void main(String[] args) {
+System.out.printIn("HelloWorld");
+}
+} 
